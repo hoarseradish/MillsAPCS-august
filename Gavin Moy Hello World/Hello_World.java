0a2bf651b89fe703d868fb25ecf6a1bc6440c9e6
hoarseradish <trex_gavin@yahoo.com>
2015-08-17 18:10:09 -0700

    hello world
    says hello world

---
+++ Gavin Moy Hello World/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * says hello world
+ * 
+ * @author (Gavin Moy) 
+ * @version (8/17/15)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Gavin>"); //Display the string
+    }
+}
