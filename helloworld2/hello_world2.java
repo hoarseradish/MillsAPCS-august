d56bb8a147142d3ee16899225f9a0312721c4cad
hoarseradish <trex_gavin@yahoo.com>
2015-08-18 09:20:51 -0700

    8/18/15 "hello world demonstration"
    hello world

---
+++ helloworld2/hello_world2.java
@@ -0,0 +1,15 @@
+
+/**
+ * first java code. says hello world.
+ * 
+ * @author (gavin moy) 
+ * @version (8/18/15)
+ */
+public class hello_world2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Gavin Moy>");
+
+    }
+}
