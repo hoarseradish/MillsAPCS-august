da683e41f302cf32a26b10f6f137663c9f152686
hoarseradish <trex_gavin@yahoo.com>
2015-08-21 08:43:48 -0700

    pikachu
    8/21/15

---
+++ chu/chu.java
@@ -0,0 +1,16 @@
+
+/**
+ * Pikachu
+ * 
+ * @author (Gavin Moy) 
+ * @version (8/21/15)
+ */
+public class chu
+{
+   public static void main(String[] args) {
+       System.out.println("Pikachu welcomes you to the world of Pokemon!");
+       System.out.println("\t  (\\__/)");
+       System.out.println("\t  (o^.^)");
+       System.out.println("\t z(_(\")(\")");
+    }
+}
